4ce0624c19aa0ced571359ae95c507ae29ce7da0
Christian Salazar <ccsalazarj@gmail.com>
2018-01-08 17:11:09 -0500

    Se agregan comentarios

--- Calculadora/Calculadora/frmCalculadora.cpp
+++ Calculadora/Calculadora/frmCalculadora.cpp
@@ -2,6 +2,17 @@
 
 //Necesario para agregar la interfaz grafica al codigo.
 
+
+/*
+
+Autores     : 	Juan David Correa 
+		  		Mateo Pulido
+		  		Christian Salazar 
+Fecha       : 2016/10/21
+Descripcion : Calculadora hecha con procedimientos en Ensamblador y el C++ para el front		  
+
+*/
+
 using namespace System;
 
 using namespace System ::Windows::Forms;
